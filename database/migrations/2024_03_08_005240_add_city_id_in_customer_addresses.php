0453a61263587be83f9f54b2f05b117bd550e3ae
cipherbrainstechnologies <info@cipherbrains.com>
2024-03-08 17:35:38 +0530

    add new migration

---
+++ database/migrations/2024_03_08_005240_add_city_id_in_customer_addresses.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customer_addresses', function (Blueprint $table) {
+            $table->bigInteger('zipcode_id');
+            $table->timestamp('delivery_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('customer_addresses', function (Blueprint $table) {
+             
+            // Drop the columns
+            $table->dropColumn('delivery_date');
+            $table->dropColumn('zipcode_id');
+        });
+    }
+};
