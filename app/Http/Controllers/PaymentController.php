c3f0d51c983a7c41bc6910a0f59e36c6a1463cc9
Krunal Patel <krunal@silverwebbuzz.com>
2024-03-19 10:43:01 +0530

    Fixed

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -291,6 +291,7 @@ class PaymentController extends Controller
         userSubscription::create($user_subscription_data);
         $order = Order::find($order->id);
         $order->transaction_reference = $res->id;
+        $order->payment_status = 'paid';
         $order->save();
         
         if(!empty($res->links[0]->href)) {
