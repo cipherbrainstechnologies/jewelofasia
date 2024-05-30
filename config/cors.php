1f3ba3234ee829723aa17360a0979d4351315d16
Love Chauhan <info@cipherbrains.com>
2024-05-30 16:12:49 +0530

    htaccess and cors file updated

--- config/cors.php
+++ config/cors.php
@@ -1,33 +1,53 @@
 <?php
 
+// return [
+
+//     /*
+//     |--------------------------------------------------------------------------
+//     | Cross-Origin Resource Sharing (CORS) Configuration
+//     |--------------------------------------------------------------------------
+//     |
+//     | Here you may configure your settings for cross-origin resource sharing
+//     | or "CORS". This determines what cross-origin operations may execute
+//     | in web browsers. You are free to adjust these settings as needed.
+//     |
+//     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+//     |
+//     */
+
+//     'paths' => ['api/*', 'storage/*'],
+
+//     'allowed_methods' => ['*'],
+
+//     'allowed_origins' => ['https://jewelsofasia.app'],
+
+//     'allowed_origins_patterns' => [],
+
+//     'allowed_headers' => ['*'],
+
+//     'exposed_headers' => [],
+
+//     'max_age' => false,
+
+//     'supports_credentials' => false,
+
+// ];
+
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+    'paths' => ['api/*', 'storage/app/public/profile/*'],
 
-    'paths' => ['api/*', 'storage/*'],
-
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
     'allowed_origins' => ['https://jewelsofasia.app'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
 
     'exposed_headers' => [],
 
-    'max_age' => false,
+    'max_age' => 0,
 
     'supports_credentials' => false,
 
