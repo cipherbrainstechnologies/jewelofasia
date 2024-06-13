5ca119c80735594eca6ef39e6cc0a5ec3cc527ab
Love Chauhan <info@cipherbrains.com>
2024-06-13 21:43:58 +0530

    BKash file changes

--- app/Http/Controllers/BkashPaymentController.php
+++ app/Http/Controllers/BkashPaymentController.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
 namespace App\Http\Controllers;
 
@@ -178,6 +178,193 @@ class BkashPaymentController extends Controller
                 call_user_func($payment_data->failure_hook, $payment_data);
             }
             return $this->payment_response($payment_data,'fail');
+        }
+    }
+
+
+} -->
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PaymentRequest;
+use App\Traits\Processor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+//use app\User;
+
+class BkashPaymentController extends Controller
+{
+    use Processor;
+
+    private $config_values;
+    private $base_url;
+    private $app_key;
+    private $app_secret;
+    private $username;
+    private $password;
+    private PaymentRequest $payment;
+    private $user;
+
+    public function __construct(PaymentRequest $payment)
+    {
+        $config = $this->payment_config('bkash', 'payment_config');
+        if (!is_null($config) && $config->mode == 'live') {
+            $this->config_values = json_decode($config->live_values);
+        } elseif (!is_null($config) && $config->mode == 'test') {
+            $this->config_values = json_decode($config->test_values);
+        }
+
+        if ($config) {
+            //$this->app_key = $this->config_values->app_key;
+            $this->app_secret = $this->config_values->app_secret;
+            $this->username = $this->config_values->username;
+            $this->password = $this->config_values->password;
+            $this->base_url = ($config->mode == 'live') ? 'https://global-api.afterpay.com' : 'https://global-api-sandbox.afterpay.com';
+        }
+
+        $this->payment = $payment;
+       
+    }
+
+    public function getToken()
+    {
+        $url = "https://merchant-auth-sandbox.afterpay.com/v2/oauth2/token";
+        $post_token = array(
+           'grant_type' => 'client_credentials',
+            'client_id' => $this->password,
+            'client_secret' => $this->$this->app_secret,
+            "scop"=> 'merchant_api_v2'
+        );
+       
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post_fields));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/x-www-form-urlencoded'
+        ]);
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        return json_decode($response, true);
+        
+    }
+
+    public function make_tokenize_payment(Request $request)
+    {
+       $validator = Validator::make($request->all(), [
+            'payment_id' => 'required|uuid'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($this->response_formatter(GATEWAYS_DEFAULT_400, null, $this->error_processor($validator)), 400);
+        }
+
+        $data = $this->payment::where(['id' => $request['payment_id'], 'is_paid' => 0])->first();
+        if (!isset($data)) {
+            return response()->json($this->response_formatter(GATEWAYS_DEFAULT_204), 200);
+        }
+
+        // $response = $this->getToken();
+        $auth =  base64_encode($this->password . ':' . $this->app_secret);
+        // session()->put('token', $auth);
+
+        $requestbody = [
+            'amount' => [
+                'amount' => round($data->payment_amount, 2),
+                'currency' => 'AUD' 
+            ],
+            'consumer' => [
+                'phoneNumber' => json_decode($data->payer_information)->phone,
+                'givenNames' => 'nisarg',
+                'surname' => 'parekh' ,
+            ],
+            'merchantReference' => $data->id,
+            'merchant' => [
+                'redirectConfirmUrl' => route('afterpay.callback', ['payment_id' => $data->id]),
+                'redirectCancelUrl' => route('afterpay.callback', ['payment_id' => $data->id])
+            ]
+        ];
+
+        $curl = curl_init($this->base_url . '/v2/checkouts');
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($requestbody));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Authorization: Basic ' . $auth,
+        ]);
+
+        $resultdata = curl_exec($curl);
+        // curl_close($curl);
+        // dd($resultdata);
+
+        $obj = json_decode($resultdata);
+
+        if (isset($obj->token)) {
+            session()->put('orderToken', $obj->token);
+            return redirect()->away($obj->redirectCheckoutUrl);
+        } else {
+            return response()->json(['error' => 'Failed to initiate payment'], 500);
+        }
+       
+    }
+
+    public function callback(Request $request)
+    {
+        $orderToken = session()->get('orderToken');
+        $auth = base64_encode($this->password . ':' . $this->app_secret);
+
+        $url = $this->base_url . '/v2/payments/capture';
+        $request_body = [
+            'token' => $orderToken,
+            'merchantReference' => $request->input('payment_id')
+        ];
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($request_body));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Authorization: Basic ' . $auth,
+        ]);
+
+        $resultdata = curl_exec($curl);
+        curl_close($curl);
+
+        $obj = json_decode($resultdata);
+
+    // Log the entire response for debugging
+        \Log::info('Afterpay API Response: ', (array)$obj);
+
+    // Check if the response is valid and contains the expected properties
+        if (is_object($obj) && isset($obj->status)) {
+            if ($obj->status == 'APPROVED') {
+                $this->payment::where(['id' => $request->input('payment_id')])->update([
+                    'payment_method' => 'afterpay',
+                    'is_paid' => 1,
+                    'transaction_id' => $obj->id ?? null,
+                ]);
+
+                $data = $this->payment::where(['id' => $request->input('payment_id')])->first();
+
+                if (isset($data) && function_exists($data->success_hook)) {
+                    call_user_func($data->success_hook, $data);
+                }
+
+                return $this->payment_response($data, 'success');
+            } 
+        }
+        else {
+           $payment_data = $this->payment::where(['id' => $request->input('payment_id')])->first();
+           if (isset($payment_data) && function_exists($payment_data->failure_hook)) {
+               call_user_func($payment_data->failure_hook, $payment_data);
+           }
+           return $this->payment_response($payment_data, 'fail');
         }
     }
 
