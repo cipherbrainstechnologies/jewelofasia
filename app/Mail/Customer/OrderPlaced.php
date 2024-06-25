078162832fe7fc5a228bac284e7f1580ce107711
nisarg1997501 <71384779+cipherbrainstechnologies@users.noreply.github.com>
2024-06-25 15:08:13 +0530

    order  logs 25/06/2024

--- app/Mail/Customer/OrderPlaced.php
+++ app/Mail/Customer/OrderPlaced.php
@@ -32,6 +32,7 @@ class OrderPlaced extends Mailable
     {
         $order_id = $this->order_id;
         $order = Order::with('delivery_address')->find($order_id);
+        \Log::info('order id done : ' . $order_id);
         return $this->view('email-templates.customer.order-placed', ['order' => $order]);
     }
 }
